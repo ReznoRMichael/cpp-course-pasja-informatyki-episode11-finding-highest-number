5ab8e5d9b539476ff59808f2bf793e90bb08d534
ReznoRMichael <mic3arena@gmail.com>
2019-05-02 00:15:47 +0200

    first added to repository

---
+++ README.md
@@ -0,0 +1,13 @@
+# Learning basics of C++ - finding the highest number
+> Training course - Pasja Informatyki, C++ course, episode 11 ( [Link](https://www.youtube.com/watch?v=63QjKBcIySs) )
+
+## General info
+Finding the highest of the inputted numbers. Two different ways of solving the same problem - simple and clean vs complicated.
+
+## Technologies
+* C++
+* CodeBlocks (IDE)
+* GNU GCC Compiler (minGW)
+
+## Contact
+Created by [ReznoRMichael](https://github.com/ReznoRMichael)
---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int a,b,c,m;
+
+int main()
+{
+    cout << "Podaj 3 liczby rozdzielone spacja: ";
+    cin >> a >> b >> c;
+
+    m=a;
+
+    if (b>m) m=b;
+    if (c>m) m=c;
+
+    cout << "Najwieksza liczba to: " << m;
+
+/*  // nieoptymalne rozwiazanie
+    if ((a>=b) && (a>=c))
+        cout << "Najwieksza liczba to " << a;
+    else if ((b>=a) && (b>=c))
+        cout << "Najwieksza liczba to " << b;
+    else if ((c>=a) && (c>=b))
+        cout << "Najwieksza liczba to " << c;
+*/
+
+    return 0;
+}
